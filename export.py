8bcc8e688d66c96e87d006fc60b801755e696e4a
unknown <vardalagoy@gmail.com>
2019-06-01 23:03:36 -0700

    Added login and started export methods for export_video.py

--- export.py
+++ export.py
@@ -1,12 +1,65 @@
+import time 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
+class ExportVideo():
+    def __init__(self, email, password):
+        self.browser = webdriver.Chrome()
 
-driver.get("https://www.twitch.tv/robosteph/manager")
+        self.email = email
+        self.password = password
 
-print(driver.title)
+    def sign_in(self):
+        self.browser.get("https://www.twitch.tv/login")
+        email_input = self.browser.find_element_by_xpath("/html/body/div[2]/div/div/div/div[1]/div/div/form/div/div[1]/div/div[2]/input")
+        pass_input = self.browser.find_element_by_xpath("/html/body/div[2]/div/div/div/div[1]/div/div/form/div/div[2]/div/div[1]/div[2]/div[1]/input")
+
+        email_input.send_keys(self.email)
+        pass_input.send_keys(self.password)
+        pass_input.send_keys(Keys.ENTER)
+        time.sleep(3)
+
+
+    def export_video(self):
+        self.browser.get("https://www.twitch.tv/robosteph/manager")
+        time.sleep(3)
+        #selecting by page position? Chose third down to avoid breaking affiliate 24hr rules
+        menu_button = self.browser.find_element_by_xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/div[3]/div/div/div/div[1]/div/div/div[5]/div[3]/div/div[1]/a/div/div")
+        menu_button.click()
+
+        #date for this vid - Needs to be tested, was getting captcha
+        #//*[@id="root"]/div/div[2]/div[2]/div/div/div[3]/div/div/div/div[1]/div/div/div[5]/div[3]/div/div[1]/a/div/div/div[2]/div/div/div/div[1]
+        date = self.browser.find_element_by_xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/div[3]/div/div/div/div[1]/div/div/div[5]/div[3]/div/div[1]/a/div/div/div[2]/div/div/div/div[1]").getText()
+        print("Date: " + date)
+
+    # def close_browser
+    # def exit
+
+
+
+f = open("user.config", "r")
+l = f.readlines()
+email = l[0]
+password = l[1]
+
+
+e = ExportVideo(email, password)
+e.sign_in()
+e.export_video()
+
+
+
+
+
+
+
+# driver = webdriver.Chrome()
+
+# driver.get("https://www.twitch.tv/robosteph/manager")
+
+# print(driver.title)
 
 # inputElement = driver.find_element_by_name("q")
 
@@ -26,3 +79,6 @@ print(driver.title)
 
 ####NOTES
 #Look into using Get Video API to get video info for posting
+
+
+#<div data-a-target="login-username-input" class="tw-relative"><input type="text" class="tw-block tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-border-top-left-radius-medium tw-border-top-right-radius-medium tw-font-size-6 tw-full-width tw-input tw-pd-l-1 tw-pd-r-1 tw-pd-y-05" autocapitalize="off" autocorrect="off" autocomplete="username" data-a-target="tw-input" value=""></div>
--- video_info.py
+++ video_info.py
@@ -41,11 +41,3 @@ print_list()
 
 # print(video_dict)
 
-
-# {'id': '429360909', 'user_id': '102812161', 'user_name': 'RoboSteph', 'title': 'Apex', 'description': '', 'created_at': '2019-05-24T21:59:55Z', 'published_at': '2019-05-24T21:59:55Z', 'url': 'https://www.twitch.tv/videos/429360909', 'thumbnail_url': 'https://static-cdn.jtvnw.net/s3_vods/7a7e93b6ab17ad0908b4_robosteph_34252272320_1209429564/thumb/thumb0-%{width}x%{height}.jpg', 'viewable': 'public', 'view_count': 0, 'language': 'en', 'type': 'archive', 'duration': '8h26m36s'} 
-
-#Get id from username
-#curl -H "Client-ID: cglhpp9jnvcuzlk6gppqfya9t56u40" -X GET "https://api.twitch.tv/helix/users?login=robosteph"
-
-#Get videos 
-#curl -H "Client-ID: cglhpp9jnvcuzlk6gppqfya9t56u40" -X GET "https://api.twitch.tv/helix/videos?user_id=102812161"

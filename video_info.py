1cfda66694cd67f5ac33f6b555bad48b08a7933d
unknown <vardalagoy@gmail.com>
2019-06-28 11:51:10 -0700

    Added info.config to store user and client IDs for video_info.py

--- video_info.py
+++ video_info.py
@@ -1,19 +1,26 @@
 #Stephanie Simpler
 #5-25-2019
 #Getting video info from Twitch
-#note - First request gets 20 most recent videos from the list, next request picks up where that left off
-#TODO - pull user and client id from file   
-
+#note - First request gets 20 most recent videos from the list (first 'page'). Next request uses the returned cursor to get older videos (second 'page').
+#TODO - currently covers two pages because our videos haven't reached three pages yet
 
 import requests
 import json
 
-url = "https://api.twitch.tv/helix/videos?user_id=102812161"
-headers = {"Client-ID" : "cglhpp9jnvcuzlk6gppqfya9t56u40"}
+
+f = open("info.config", "r")
+l = f.readlines()
+uid = l[0].rstrip()
+client_id = l[1].rstrip()
+
+url = "https://api.twitch.tv/helix/videos?user_id=" + uid
+headers = {"Client-ID" : client_id}
 
 def print_list():
     r = requests.get(url, headers=headers)
     video_dict = r.json()
+
+    # print(video_dict)
 
     data_list = video_dict["data"]
     pagin = video_dict["pagination"] 
@@ -23,11 +30,13 @@ def print_list():
     for x in data_list:
     	print(x["user_name"])
     	print(x["title"])
-    	print(x["published_at"]) #Don't forget to check published_at before exporting!
+    	print(x["published_at"]) #Don't forget to check published_at before exporting, 24 hours or older
     	print(x["duration"])
     	print("-------------------------------------------------------------")
     	count +=1
-    print("Count for this page: " + count)
+    print("-----")
+    print("Count for this page: " + str(count))
+    print("-----")
 
     return page
 
